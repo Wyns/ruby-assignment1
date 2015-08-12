ef8b03c4ac9ee062d182bd8bbff80a1b50563ff6
Wyns <pauwains@gmail.com>
2015-08-12 12:41:54 +0300

    added palindrome

---
+++ palindrome.rb
@@ -0,0 +1,23 @@
+def palindrome?(string)
+
+i = 0
+
+while i < string.length
+
+
+
+if string[i] != string[(string.length - 1) - i]
+return false
+
+end
+
+i += 1
+end
+
+return true
+end
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+puts('palindrome?("abc") == false: ' + (palindrome?('abc') == false).to_s)
+puts('palindrome?("abcba") == true: ' + (palindrome?('abcba') == true).to_s)
+puts('palindrome?("z") == true: ' + (palindrome?('z') == true).to_s)
